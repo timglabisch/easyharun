34c642cc82c367a743cb80b58646b1333dca67a9
timglabisch <tim@ainfach.de>
2023-06-04 20:30:31 +0200

    kindA works

--- easyact/src/actor/wrapper.rs
+++ easyact/src/actor/wrapper.rs
@@ -1,109 +1,139 @@
-use std::future::{Future, poll_fn};
-use std::marker::PhantomData;
+use std::fmt::{Debug, Display, Formatter};
 use std::pin::Pin;
-use std::task::{Context, Poll};
-use std::thread::sleep;
+use std::sync::atomic::{AtomicU64, Ordering};
 use async_trait::async_trait;
-use futures::future::select_all;
 use futures::select;
 use pin_project_lite::pin_project;
+use tokio::sync::mpsc::Receiver;
+use tokio::sync::mpsc::channel;
+use tokio::sync::oneshot::{Sender};
 use tokio_util::sync::CancellationToken;
-use tracing::trace;
+use tracing::warn;
 
-#[async_trait]
-pub trait Actor
-{
-    type MSG : Sized + Send;
+const ACTOR_ID_GEN : AtomicU64 = AtomicU64::new(0);
 
-    async fn on_msg(&mut self) {
-        ()
+#[derive(Debug, Clone)]
+pub struct ActorId(pub u64);
+
+impl Display for ActorId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.0.to_string().as_str())
     }
 }
 
-pub struct ActorParent {
-    cancellation_token: CancellationToken,
+#[derive(Debug)]
+pub struct ActorMsgPing {
+    actor_id: ActorId,
+    shot: Sender<ActorMsgPingResponse>
 }
 
+#[derive(Debug)]
 pub struct ActorMsgPingResponse {
-    id: usize,
+    actor_id: ActorId,
 }
 
-pub struct ActorMsgPingRequest {
-    oneshot: ::tokio::sync::oneshot::Sender<ActorMsgPingResponse>,
-    id: usize,
+#[derive(Debug)]
+enum ActorMsg<MSG> {
+    Msg(MSG),
+    Ping(ActorMsgPing),
 }
 
-pub enum ActorMsg<T> {
-    MSG(T),
-    KILL,
-    PING(ActorMsgPingRequest),
+#[derive(Debug, Default)]
+pub struct ActorStateMetrics {
+    msg_msg: u64,
+    msg_ping: u64,
+    errors: u64,
 }
 
-    pub struct ActorWrapper<ACTOR>
-    where ACTOR : Actor,
-    {
-        pub inbox: ::tokio::sync::mpsc::UnboundedReceiver<ActorMsg<ACTOR::MSG>>,
-        pub shutdown: bool,
-        pub parent: Option<ActorParent>,
-        pub actor: ACTOR,
-        pub future_drive_onmsg: Option<Pin<Box<dyn Future<Output = ()> + Send>>>,
+
+pub struct ActorState<MSG> where MSG: Send, MSG : Sync, MSG: Sized, MSG: Unpin {
+    id: ActorId,
+    name: &'static str,
+    parent_cancellation_token: Option<CancellationToken>,
+    cancellation_token: CancellationToken,
+    inbox: Receiver<ActorMsg<MSG>>,
+    metrics: ActorStateMetrics,
+}
+
+pub struct ActorStateHandle<MSG> where MSG: Send, MSG : Sync, MSG: Sized, MSG: Unpin {
+    id: ActorId,
+    sender: ::tokio::sync::mpsc::Sender<ActorMsg<MSG>>
+}
+
+impl<MSG> ActorState<MSG> where MSG: Send, MSG : Sync, MSG: Sized, MSG: Unpin {
+
+    pub fn new_root(name: &'static str) -> (ActorStateHandle<MSG>, Self) {
+        Self::new(name, None)
     }
 
+    pub fn new_child(name: &'static str, parent_cancellation_token: CancellationToken) -> (ActorStateHandle<MSG>, Self) {
+        Self::new(name, Some(parent_cancellation_token))
+    }
 
-impl<'a, ACTOR> Future for ActorWrapper<ACTOR>
-    where ACTOR: Actor + Send + Unpin
-{
-    type Output = ();
+    fn new(name: &'static str, parent_cancellation_token: Option<CancellationToken>) -> (ActorStateHandle<MSG>, Self) {
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output>
-    {
-        //let mut this = self.project();
-        // let mut inbox = this.inbox;
-        // let actor = this.actor;
+        let (inbox_sender, inbox) = channel::<ActorMsg<MSG>>(1000);
 
-        // let this = self.as_mut();
+        let id = ActorId(ACTOR_ID_GEN.fetch_add(1, Ordering::Relaxed));
 
-        let this = Pin::get_mut(self);
-
-        /*
-        match self.future_drive_onmsg.take() {
-
-        }
-         */
-
-        match this.inbox.poll_recv(cx) {
-            Poll::Pending => {},
-            Poll::Ready(r) => {
-                let msg = match r {
-                    None => return Poll::Ready(()),
-                    Some(r) => r
-                };
-
-                match msg {
-                    ActorMsg::KILL => {
-                        return Poll::Ready(())
-                    },
-                    ActorMsg::PING(ping_response) => {
-                        match ping_response.oneshot.send(ActorMsgPingResponse {
-                            id: ping_response.id
-                        }) {
-                            Ok(_) => {},
-                            Err(e) => {
-                                trace!("could not send ping response.")
-                            }
-                        }
-                    },
-                    ActorMsg::MSG(generic_msg) => {
-
-                        let on_msg = this.actor.on_msg();
-                        this.future_drive_onmsg.insert(on_msg);
-
-                        // let poll = Pin::new(&mut on_msg).poll(cx);
-                    }
-                }
-            }
+        let s = Self {
+            id: id.clone(),
+            cancellation_token: CancellationToken::new(),
+            metrics: ActorStateMetrics::default(),
+            inbox,
+            parent_cancellation_token: None,
+            name
         };
 
-        Poll::Pending
+        (
+            ActorStateHandle {
+                id: id,
+                sender: inbox_sender
+            },
+            s
+        )
     }
+}
+
+
+#[async_trait]
+pub trait Actor{
+    type MSG: Send + Sync + Sized + Unpin + Debug;
+
+    fn get_actor_state(&mut self) -> &mut ActorState<Self::MSG>;
+
+    async fn run_loop_inner(&mut self) -> Result<(), ::anyhow::Error> {
+
+        let mut state = self.get_actor_state();
+
+        loop {
+            ::tokio::select! {
+                msg = state.inbox.recv() => {
+                    println!("msg: {:#?}", msg);
+                },
+                _ = async { state.parent_cancellation_token.as_mut().expect("crash here").cancelled().await }, if state.parent_cancellation_token.is_some() => {
+                    println!("parent cancelled. we kill ourself");
+                    return Ok(());
+                }
+            }
+        }
+    }
+
+    async fn run_loop(&mut self) {
+
+        loop {
+            match self.run_loop_inner().await {
+                Ok(_) => return,
+                Err(e) => {
+                    self.get_actor_state().metrics.errors += 1;
+                    warn!("Actor Error {}", self.get_actor_state().name);
+                }
+            };
+
+            ::tokio::time::sleep(::tokio::time::Duration::from_millis(500)).await;
+        }
+
+    }
+
+
 }
--- easyact/src/main.rs
+++ easyact/src/main.rs
@@ -1,9 +1,44 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use futures::future::join;
+use tokio::task::JoinHandle;
+use crate::actor::wrapper::{Actor, ActorState, ActorStateHandle};
+
 pub mod actor;
+
+struct ActorA {
+    actor_state: ActorState<String>
+}
+
+impl ActorA {
+    pub fn spawn() -> (JoinHandle<()>, ActorStateHandle<String>) {
+
+        let (handle, actor_state) = ActorState::new_root("ActorA");
+
+        let jh = ::tokio::spawn(async move {
+            Self {
+                actor_state
+            }.run_loop().await
+        });
+
+        (jh, handle)
+    }
+}
+
+impl Actor for ActorA {
+    type MSG = String;
+
+    fn get_actor_state(&mut self) -> &mut ActorState<Self::MSG> {
+        &mut self.actor_state
+    }
+}
 
 #[tokio::main]
 pub async fn main() {
+    let (jh_1, state_a) = ActorA::spawn();
+    let (jh_2, state_b) = ActorA::spawn();
 
+    join(jh_1, jh_2).await;
 }
+ 
